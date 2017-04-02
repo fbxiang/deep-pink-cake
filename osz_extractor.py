04db19232c7c8424a5b385381158609c8673b912
FFTypeZero <zxiong9@illinois.edu>
2017-04-01 20:13:01 -0500

    small change

--- osz_extractor.py
+++ osz_extractor.py
@@ -10,14 +10,14 @@ folder_names = [name.replace('.osz', '') for name in names]
 n = len(names)
 
 for i in range(n):
-	if '.osz' in names[i]:
-		os.mkdir(folder_names[i])
-		patoolib.extract_archive(names[i], outdir = folder_names[i])
-		subnames = os.listdir(folder_names[i])
-		for name in subnames:
-			path = folder_names[i] + '/' + name
-			if '.mp3' not in name and '.osu' not in name:
-				if os.path.isdir(path):
-					shutil.rmtree(path)
-				else:
-					os.remove(path)
+    if '.osz' in names[i]:
+        os.mkdir(folder_names[i])
+        patoolib.extract_archive(names[i], outdir = folder_names[i])
+        subnames = os.listdir(folder_names[i])
+        for name in subnames:
+            path = folder_names[i] + '/' + name
+            if '.mp3' not in name and '.osu' not in name:
+                if os.path.isdir(path):
+                    shutil.rmtree(path)
+                else:
+                    os.remove(path)
